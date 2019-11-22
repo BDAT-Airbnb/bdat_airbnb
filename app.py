0c8af5d904123a7ade5e356c1053447017196065
abinj <abinmj656@gmail.com>
2019-11-22 11:50:03 -0500

    flask-restful library imported

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import flask_restful
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 app.config['MONGO_DBNAME'] = 'bdat_airbnb'
 app.config["MONGO_URI"] = "mongodb://localhost:27017/bdat_airbnb"
@@ -43,6 +46,17 @@ def airbnb_charts():
 @app.route('/tables.html')
 def airbnb_tables():
     return render_template("tables.html")
+
+
+class Dataset(Resource):
+    def get(self):
+        return {}
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(Dataset, '/dataset')
 
 if __name__ == '__main__':
     app.run()
